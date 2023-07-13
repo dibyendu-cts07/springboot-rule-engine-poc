6f897e5eb77c2dbb6429d84fcb9a46946f73860f
dibyendu-cts07 <dibyendu.cts07@gmail.com>
2023-07-13 14:50:34 +0530

    initial code

---
+++ src/main/java/com/tutorial/rule/SpringbootRuleEnginePocApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.rule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRuleEnginePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRuleEnginePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/rule/SpringbootRuleEnginePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.rule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRuleEnginePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
